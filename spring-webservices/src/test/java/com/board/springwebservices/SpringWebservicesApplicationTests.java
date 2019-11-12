7469819b745ea7c606b0a42208bfa4681e074b05
KDM_korea <kdm_korea@naver.com>
2019-11-12 22:28:27 +0900

    Create: Web service project

---
+++ spring-webservices/src/test/java/com/board/springwebservices/SpringWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.springwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
