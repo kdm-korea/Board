33ced3f0275ccbc50ce07d242e4f1fde3bb496e1
KDM_korea <kdm_korea@naver.com>
2019-11-19 13:26:01 +0900

    Feat: DAO Class

---
+++ spring-webservices/src/main/java/com/board/webservice/domain/posts/Posts.java
@@ -0,0 +1,30 @@
+package com.board.webservice.domain.posts;
+
+import lombok.AccessLevel;
+import lombok.*;
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
