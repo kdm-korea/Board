32bae58e537fba9cca918d9a37834e45de179689
KDM_korea <kdm_korea@naver.com>
2019-11-21 21:26:15 +0900

    TDD: assert BaseTimeEntity_등록

--- spring-webservices/src/test/java/com/board/webservice/domain/PostsRepositoryTest.java
+++ spring-webservices/src/test/java/com/board/webservice/domain/PostsRepositoryTest.java
@@ -4,17 +4,18 @@ import com.board.webservice.domain.posts.Posts;
 import com.board.webservice.domain.posts.PostsRepository;
 import org.junit.After;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import static org.hamcrest.CoreMatchers.is;
-//import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -44,5 +45,21 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle(), is("테스트 게시글"));
         assertThat(posts.getContent(), is("테스트 본문"));
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+            .title("테스트 게시글")
+            .content("테스트 본문")
+            .author("kdm_korea@naver.com")
+            .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
     }
 }
