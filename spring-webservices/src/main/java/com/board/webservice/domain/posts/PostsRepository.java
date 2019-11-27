eed447c619588cc260e7bc95125c0728b30a07a8
KDM_korea <kdm_korea@naver.com>
2019-11-27 20:51:28 +0900

    Feat:  DAO, DB layer access interface with using JpaRepository

---
+++ spring-webservices/src/main/java/com/board/webservice/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.board.webservice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
