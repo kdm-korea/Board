57deaa53fc2ba365b3c734bec073fb17c3e4b7d4
KDM_korea <kdm_korea@naver.com>
2019-12-06 21:36:09 +0900

    Feat: package modified

--- spring-webservice/src/main/java/com/board/webservice/Application.java
+++ spring-webservice/src/main/java/com/board/webservice/Application.java
@@ -1,9 +1,11 @@
-package com.board.webservices;
+package com.board.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
     public static void main(String[] args) {
