9b7e2ca95d1bbdbc82cd9a826d359b3ecea13e80
KDM_korea <kdm_korea@naver.com>
2019-12-06 11:04:21 +0900

    Feat: directory modified

--- spring-webservice/src/main/java/com/board/webservices/Application.java
+++ spring-webservice/src/main/java/com/board/webservices/Application.java
@@ -1,4 +1,4 @@
-package com.board.springwebservice;
+package com.board.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
