7e6c23f3f85f7b81cff60eefc12466e9c7022c3c
KDM_korea <kdm_korea@naver.com>
2019-12-07 19:25:37 +0900

    TDD: Dto, value compare Service throw transaction data

--- spring-webservice/src/test/java/com/board/webservice/service/PostServiceTest.java
+++ spring-webservice/src/test/java/com/board/webservice/service/PostServiceTest.java
@@ -24,10 +24,10 @@ public class PostServiceTest {
     @Autowired
     private PostsRepository postsRepository;
 
-    @After
-    public void claenup(){
-        postsRepository.deleteAll();
-    }
+    //@After
+    //public void claenup(){
+    //    postsRepository.deleteAll();
+    //}
 
     @Test
     public void DTO데이터가_posts테이블에_저장된다(){
@@ -43,7 +43,7 @@ public class PostServiceTest {
 
         //then
         Posts post = postsRepository.findAll().get(0);
-        assertThat(post.getAuthor()).isEqualTo(dto.getAuthor());
+        assertThat(post.getAuthor()).isEqualTo("kdm_korea@naver.com");
         assertThat(post.getContent()).isEqualTo(dto.getContent());
         assertThat(post.getTitle()).isEqualTo(dto.getTitle());
     }
