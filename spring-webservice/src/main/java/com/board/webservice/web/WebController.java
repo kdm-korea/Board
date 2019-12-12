d6149bf07f4ba6306c71d74ac2278722425b62b4
KDM_korea <kdm_korea@naver.com>
2019-12-12 20:44:00 +0900

    Feat: Connect main.hbs

--- spring-webservice/src/main/java/com/board/webservice/web/WebController.java
+++ spring-webservice/src/main/java/com/board/webservice/web/WebController.java
@@ -1,15 +1,21 @@
 package com.board.webservice.web;
 
+import com.board.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class WebController {
 
+    private PostsService postsService;
+
     @GetMapping("/")
-    public String main(){
+    public String main(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
+        //System.out.println(model.getAttribute("posts"));
         return "main";
     }
 }
