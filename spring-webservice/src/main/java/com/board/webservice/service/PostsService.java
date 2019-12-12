52186b4337c4cbaf228286e2e3c57ff88327bbee
KDM_korea <kdm_korea@naver.com>
2019-12-12 20:42:42 +0900

    Feat: Board list dto

--- spring-webservice/src/main/java/com/board/webservice/service/PostsService.java
+++ spring-webservice/src/main/java/com/board/webservice/service/PostsService.java
@@ -1,5 +1,6 @@
 package com.board.webservice.service;
 
+import com.board.webservice.dto.PostsMainResponseDto;
 import com.board.webservice.dto.posts.PostsSaveRequestDto;
 import com.board.webservice.web.domain.posts.PostsRepository;
 import com.sun.tools.javac.jvm.ByteCodes;
@@ -7,6 +8,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -16,5 +19,13 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<PostsMainResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc()
+//                .map(posts -> new PostsMainResponseDto(posts))
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
