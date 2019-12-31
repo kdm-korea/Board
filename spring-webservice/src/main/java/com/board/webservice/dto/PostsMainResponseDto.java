6f0e8122e69f925ae3c72e31dfbe7bc1501bd0ce
KDM_korea <kdm_korea@naver.com>
2019-12-31 16:09:36 +0900

    Feat: sringboot DI builder add

--- spring-webservice/src/main/java/com/board/webservice/dto/PostsMainResponseDto.java
+++ spring-webservice/src/main/java/com/board/webservice/dto/PostsMainResponseDto.java
@@ -1,17 +1,22 @@
 package com.board.webservice.dto;
 
 import com.board.webservice.web.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
+@Getter
 public class PostsMainResponseDto {
     private Long id;
     private String title;
     private String author;
     private String modifiedDate;
 
+
+    @Builder
     public PostsMainResponseDto(Posts entity){
         id = entity.getId();
         title = entity.getTitle();
@@ -26,5 +31,12 @@ public class PostsMainResponseDto {
                 .orElse("");
     }
 
-
+//    private String toStringDateTime(LocalDateTime localDateTime){
+//        if(localDateTime == null){
+//            return "";
+//        }
+//
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        return formatter.format(localDateTime);
+//    }
 }
